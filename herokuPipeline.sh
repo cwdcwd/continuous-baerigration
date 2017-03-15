2eb8c7eb4c2d381950890ab088006e9093239edb
CWD <cwd@cloudspokes.com>
2017-03-15 12:56:05 -0400

    Revert "setting up a heroku pipeline builder script"
    This reverts commit b8ba59989c4b99e85fd11fcb1b064ad0fb81050a.

--- herokuPipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#heroku create $1-dev
-#heroku create $1-stage
-#heroku create $1-prod
-
-origin=$(git config --get remote.origin.url)
-echo "creating pipeline for $1 from $origin"
-heroku pipelines:setup $1 $origin
