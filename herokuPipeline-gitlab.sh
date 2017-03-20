11cd3e2c0e9859b7907e1a5d0a8524932cb83838
CWD <cwd@cloudspokes.com>
2017-03-20 14:23:18 -0400

    limiting name of pipeline set up as per heroku  name limits

--- herokuPipeline-gitlab.sh
+++ herokuPipeline-gitlab.sh
@@ -2,16 +2,24 @@
 
 APPS=( $1-dev $1-stage $1-prod)
 STAGES=("development" "staging" "production")
+NAME=$1
+NAME_LEN=${#NAME}
+echo "Setting up apps & pipeline: '${NAME}'"
+
+if [ $NAME_LEN -gt 24 ]; then
+  echo "'${NAME}' is too long. Max 24 chars."
+  exit 1
+fi
 
 for ((i = 0; i < ${#APPS[@]}; i++)); do
-  echo "creating app ${APPS[i]} for ${STAGES[i]}"
+  echo "Creating app ${APPS[i]} for ${STAGES[i]}"
   heroku create ${APPS[i]}
 
   if [ $i -eq 0 ]; then
-    echo "Creating heroku pipeline named ${1}"
-    heroku pipelines:create $1 -s ${STAGES[i]} --app ${APPS[i]}
+    echo "Creating heroku pipeline named ${NAME}"
+    heroku pipelines:create $NAME -s ${STAGES[i]} --app ${APPS[i]}
   else
-    echo "adding ${APPS[i]} to pipeline at ${STAGES[i]}"
-    heroku pipelines:add $1 -a ${APPS[i]} -s ${STAGES[i]}
+    echo "Adding ${APPS[i]} to pipeline at ${STAGES[i]}"
+    heroku pipelines:add $NAME -a ${APPS[i]} -s ${STAGES[i]}
   fi
 done
